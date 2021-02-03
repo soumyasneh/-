66d1caadb5c5320e998caa8974323228c87aa250
soumyasneh <71879106+soumyasneh@users.noreply.github.com>
2021-02-03 19:30:24 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,121 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+var hero, grlmg, grlflymg;
+var gradent;
+var alien, alienimg;
+
+ //aliengroup = new Group();
+
+
+function preload()
+{
+grlmg = loadAnimation("download (2).png", "download (3).png");
+gradent = loadImage("mm.jpg");
+grlflymg = loadAnimation("download (2) - Copy.png");
+grlatt = loadAnimation("att.png");
+alienimg = loadImage("UFO.png");	
+auzar = loadImage("tool.png");
+}
+
+function setup() {
+	createCanvas(1400, displayHeight*10);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+
+	
+
+	hero = createSprite(500, displayHeight*9.67, 20, 20);
+	hero.addAnimation("grll", grlmg);
+	hero.addAnimation("boy", grlflymg);
+	hero.addAnimation("boyatt", grlatt);
+	hero.scale=1;
+
+	
+
+    //alien = createSprite();
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(gradent);
+
+ // tt.visible = false;
+  
+  if(keyDown("up")){
+	 
+	hero.y=hero.y-34;  
+	hero.changeAnimation("boy", grlflymg);
+	hero.scale=1;
+	}
+
+  if(keyDown("down")){
+	 
+	hero.y=hero.y+34;  
+	hero.changeAnimation("boy", grlflymg);
+	hero.scale=1;
+	}
+
+ if(keyDown("left")){
+	 
+	hero.x=hero.x-8;  
+	//hero.changeAnimation("boy", grlflymg);
+	}
+ if(keyDown("right")){
+	 
+	hero.x=hero.x+8;  
+	//hero.changeAnimation("boy", grlflymg);
+	}
+
+if (frameCount%100 === 0){
+	alien = createSprite(random(600,1300), random(100,500),100,100);
+	alien.addImage(alienimg);
+	alien.scale=1.8;
+	alien.debug=true;
+	//alien.setCollider("rectangle",0,0,130,20,130);
+	alien.lifetime=150;
+
+}
+
+if(keyDown("A")){
+	   
+	hero.changeAnimation("boyatt",grlatt);
+	hero.scale=0.35;
+
+	tt = createSprite(hero.x+30, hero.y-30);
+	tt.addImage(auzar);
+	tt.scale=0.5;
+	tt.debug=true;
+	//tt.visible = true;
+	tt.velocityX=10;
+	
+	tt.collide(alien);
+	tt.depth=alien.depth;
+
+	if(alien.isTouching(tt))
+	{
+		alien.destroyed();
+	}
+
+	}
+
+
+
+
+  drawSprites();
+ 
+}
+
+
+
